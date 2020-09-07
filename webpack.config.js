682929f6635a3a82c255fa542b5701c62983c626
nurislam-ab <abdurasulovnurislam@gmail.com>
2020-09-08 03:03:31 +0500

    Update configes 1. Add dependencies 2. Add rules

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,27 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [{
+      test: /\.s?[a?c]ss$/i,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader',
+      ],
+    },
+    {
+      test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/',
+          },
+        },
+      ],
+    },
+    ],
+  },
 };
