ca6dbbc9d90c653751cc5c69ab56ba6f2965febb
nurislam-ab <abdurasulovnurislam@gmail.com>
2020-09-11 02:13:56 +0500

    Add method callings and logic to switch between tabs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,62 @@
 import Home from './home';
-import UIComponents from './components'
+import Menu from './menu';
+import Contact from './contacts';
+import Nav from './nav';
+import Footer from './footer';
 
-const getContent = () => {
+import './assets/css/reset.scss';
+import './assets/css/styles.scss';
+
+const getHomeContent = () => {
   Home.getHomeContent();
-}
-
-window.onload = () => {
-  getContent();
 };
 
+const getMenuContent = () => {
+  Menu.getMenuContent();
+};
 
+const getContactContent = () => {
+  Contact.getContactContent();
+};
+
+const getNavigationMenu = () => {
+  document.body.appendChild(Nav.getMainNavigation());
+};
+
+const getFooter = () => {
+  document.body.appendChild(Footer.getFooter());
+};
+
+const tabsHandler = (elementId = '') => {
+  const tabHolder = document.getElementById('content');
+  switch (elementId) {
+    case 'home':
+      tabHolder.removeChild(tabHolder.firstChild);
+      getHomeContent();
+      break;
+    case 'menu':
+      tabHolder.removeChild(tabHolder.firstChild);
+      getMenuContent();
+      break;
+    case 'contact':
+      tabHolder.removeChild(tabHolder.firstChild);
+      getContactContent();
+      break;
+    default:
+      getHomeContent();
+      break;
+  }
+};
+
+window.openTab = (id) => {
+  tabsHandler(id);
+};
+
+window.onload = () => {
+  getNavigationMenu();
+  const mainContent = document.createElement('div');
+  mainContent.setAttribute('id', 'content');
+  document.body.appendChild(mainContent);
+  tabsHandler();
+  getFooter();
+};
